f98f8b305250ba89a182282f420d1a840b8fd672
mahirox36 <ragontube1@gmail.com>
2025-01-20 19:50:40 +0300

    Removed the Comments

--- classes/Features/Achievement.py
+++ classes/Features/Achievement.py
@@ -1,12 +1,7 @@
 from modules.Nexon import *
 
 
-#Ideas:
-# Title             - Description
-# The Femboy Kisser - Send Any femboy emoji
-# Meet The Creator  - Meet the owner of the bot
-# You have no life  - Send 1000 message
-# 
+
 class Achievement(commands.Cog):
     def __init__(self, client:Bot):
         self.client = client
