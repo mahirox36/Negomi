d59283d72a092f77eba863c9f8d1459f60a40ea3
mahirox36 <ragontube1@gmail.com>
2025-04-02 09:21:09 +0300

    deleted nexon

--- nexon/ext/slash_utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-nexon.ext.slash_utils
-~~~~~~~~~~~~~~~~~~~~~~~~
-An extension module to provide utilities for slash commands.
-:copyright: (c) 2022-present Nextcord Developers
-:license: MIT, see LICENSE for more details.
-"""
-
-from .slash import describe
