278bef11202d84c480c0cf1f2c3609b05758e88b
mahirox36 <ragontube1@gmail.com>
2025-05-09 10:57:42 +0300

    updated bash command

--- commands/update.bash
+++ commands/update.bash
@@ -6,8 +6,7 @@ git pull origin main || { echo "Failed to pull main repository."; exit 1; }
 
 # Update submodules
 echo "Updating submodules..."
-git submodule update --init --recursive || { echo "Failed to update submodules."; exit 1; }
-git submodule foreach git pull origin main || { echo "Failed to pull submodule updates."; exit 1; }
+git submodule update --init --recursive --remote || { echo "Failed to update submodules."; exit 1; }
 
 # Run Aerich migrations and upgrades
 echo "Running Aerich migrations and upgrades..."
